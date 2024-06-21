c928b102e51416cc441935960f983f73882a673f
victorDEV GG <vic22mj@gmail.com>
2024-06-21 14:32:11 +0000

    Update readme sql

--- demo/prueba.sql
+++ demo/prueba.sql
@@ -1,17 +1,14 @@
-DROP DATABASE prueba;
-
+Drop database prueba;
 CREATE DATABASE prueba;
 
 USE prueba;
 
---  create table materias with next columns (id_materia,materia,carrera)
 CREATE TABLE materias (
   id_materia INT AUTO_INCREMENT PRIMARY KEY,
-  materia VARCHAR(255) NOT NULL,
-  carrera VARCHAR(255) NOT NULL
+  materia VARCHAR(100) NOT NULL,
+  cuatrimestre INT NOT NULL
 );
-
-LOAD DATA LOCAL INFILE "materias.csv"
+LOAD DATA LOCAL INFILE "Base de datos - materias.csv"
 INTO TABLE materias
 COLUMNS TERMINATED BY ','
 OPTIONALLY ENCLOSED BY '"'
@@ -19,5 +16,68 @@ ESCAPED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES;
 
-SELECT * FROM materias;
+CREATE TABLE temas (
+  id_tema INT AUTO_INCREMENT PRIMARY KEY,
+  nombre_tema VARCHAR(100) NOT NULL,
+  dificultad INT NOT NULL,
+  id_materia INT,
+  FOREIGN KEY (id_materia) REFERENCES materias(id_materia)
+);
+LOAD DATA LOCAL INFILE "Base de datos - temas.csv"
+INTO TABLE temas
+COLUMNS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+ESCAPED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+CREATE TABLE preguntas (
+  id_pregunta INT AUTO_INCREMENT PRIMARY KEY,
+  pregunta TEXT NOT NULL,
+  id_tema INT,
+  calidad_prompt INT NOT NULL,
+  FOREIGN KEY (id_tema) REFERENCES temas(id_tema)
+);
+LOAD DATA LOCAL INFILE "Base de datos - preguntas.csv"
+INTO TABLE preguntas
+COLUMNS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+ESCAPED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+CREATE TABLE respuestas (
+  id_respuesta INT AUTO_INCREMENT PRIMARY KEY,
+  respuesta TEXT NOT NULL,
+  ia VARCHAR(100) NOT NULL,
+  calidad_respuesta INT NOT NULL,
+  id_pregunta INT,
+  FOREIGN KEY (id_pregunta) REFERENCES preguntas(id_pregunta)
+);
+LOAD DATA LOCAL INFILE "Base de datos - respuestas.csv"
+INTO TABLE respuestas
+COLUMNS TERMINATED BY ','
+OPTIONALLY ENCLOSED BY '"'
+ESCAPED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;
+
+
+CREATE VIEW vista_completa AS
+SELECT 
+    m.materia,
+    t.nombre_tema AS tema,
+    p.pregunta,
+    p.calidad_prompt,
+    r.respuesta,
+    r.ia,
+    r.calidad_respuesta
+FROM 
+    materias m
+JOIN 
+    temas t ON m.id_materia = t.id_materia
+JOIN 
+    preguntas p ON t.id_tema = p.id_tema
+JOIN 
+    respuestas r ON p.id_pregunta = r.id_pregunta;
 
